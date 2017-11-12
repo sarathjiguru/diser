91e50d1679d639573f2f89971e5a7acc3dda272c
sarathjiguru <sarath@peel.com>
2017-11-12 08:01:43 +0530

    adding testcases

---
+++ src/test/java/com/sarathjiguru/memory/DiMemoryTest.java
@@ -0,0 +1,48 @@
+package com.sarathjiguru.memory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sarath on 12/11/17.
+ */
+public class DiMemoryTest {
+    @Test
+    public void result() throws Exception {
+        String commandObject = "";
+        DiMemory diMemory = new DiMemory(commandObject);
+        assertEquals(diMemory.result(), "Unsupported Operation. Given Command is ");
+
+        commandObject = " $";
+        diMemory = new DiMemory(commandObject);
+        assertEquals(diMemory.result(), "Unsupported Operation. Given Command is " + commandObject);
+
+        commandObject = "set$a$b";
+        diMemory = new DiMemory(commandObject);
+        assertEquals(diMemory.result(), "Unsupported Operation. Given Command is " + commandObject);
+
+        commandObject = "get$a";
+        diMemory = new DiMemory(commandObject);
+        assertEquals(diMemory.result(), "Unsupported Operation. Given Command is " + commandObject);
+
+        commandObject = "GET$a$4";
+        diMemory = new DiMemory(commandObject);
+        assertEquals(diMemory.result(), "Unsupported Operation. Given Command is " + commandObject);
+
+        commandObject = "SET$a$";
+        diMemory = new DiMemory(commandObject);
+        assertEquals(diMemory.result(), "Unsupported Operation. Given Command is " + commandObject);
+
+        commandObject = "SET$a$4$5";
+        diMemory = new DiMemory(commandObject);
+        assertEquals(diMemory.result(), "Unsupported Operation. Given Command is " + commandObject);
+
+
+        commandObject = "GET$a$";
+        diMemory = new DiMemory(commandObject);
+        assertEquals(diMemory.result(), "key a does not exist");
+
+    }
+
+}
