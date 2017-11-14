27bdd73d3c51a5564aa3774e90d1f84749422b75
sarathjiguru <sarath@peel.com>
2017-11-15 05:25:44 +0530

    url parser for diser://<host>:<port>/

--- src/main/java/com/sarathjiguru/server/DiserUrl.java
+++ src/main/java/com/sarathjiguru/server/DiserUrl.java
@@ -4,4 +4,21 @@ package com.sarathjiguru.server;
  * Created by sarath on 15/11/17.
  */
 public class DiserUrl {
+    private final String host;
+    private final int port;
+
+    public DiserUrl(String r) {
+        String hostPort = r.replace("diser://", "").replace("/", "");
+        String[] split = hostPort.split(":");
+        this.host = split[0];
+        this.port = Integer.parseInt(split[1]);
+    }
+
+    public int port() {
+        return port;
+    }
+
+    public String host() {
+        return host;
+    }
 }
